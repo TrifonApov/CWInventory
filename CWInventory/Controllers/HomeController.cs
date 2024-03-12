021b00c16aedd1a1e656301c9e7527ec164e7efd
Trifon Apov <trifonapov@gmail.com>
2024-03-12 14:08:38 +0200

    Add Base controler

--- CWInventory/Controllers/HomeController.cs
+++ CWInventory/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using CWInventory.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,19 +7,8 @@ namespace CWInventory.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [AllowAnonymous]
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
