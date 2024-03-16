fe90cb125384cc377a5174a54b0ac0c24e3b6ee9
Trifon Apov <trifonapov@gmail.com>
2024-03-16 12:30:59 +0200

    Claims principal extention addded

---
+++ CWInventory/Extensions/ClaimsPrincipalExtentions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace CWInventory.Extensions
+{
+    public static class ClaimsPrincipalExtentions
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
