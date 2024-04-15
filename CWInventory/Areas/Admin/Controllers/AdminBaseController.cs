dfcdabe04506f17c9cfd136659e8670b9fe77960
Trifon Apov <trifonapov@gmail.com>
2024-04-15 21:34:02 +0300

    Adder admin area

---
+++ CWInventory/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static CWInventory.Core.Constants.RoleConstants;
+
+namespace CWInventory.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = AdminRole)]
+    public class AdminBaseController : Controller
+    {
+    }
+}
---
+++ CWInventory/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CWInventory.Areas.Admin.Controllers
+{
+    public class HomeController : AdminBaseController
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
